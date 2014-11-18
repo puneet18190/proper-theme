7e514d11b2118c7c47626071127a67a34f1d5318
Ritika90 <erritika.1690@gmail.com>
2014-11-18 15:59:14 +0530

    m

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,5 +6,5 @@ class Property < ActiveRecord::Base
   mount_uploader :image5, Image5Uploader
   mount_uploader :image6, Image6Uploader
   validates :name, presence: true, length: { maximum: 20 }
-  validates_presence_of :image1, :image2, :image3, :image4, :image5, :image6, :address1, :address2, :address3, :price, :postcode, :beds, :bath, :description
+  validates_presence_of :image1, :address1, :address2, :address3, :price, :postcode, :beds, :bath, :description
 end
--- app/uploaders/image1_uploader.rb
+++ app/uploaders/image1_uploader.rb
@@ -19,9 +19,9 @@ class Image1Uploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  #   #ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
+  #   "/assets" + [:thumb,"default.jpg"].compact.join('_')
+  #   # "/assets/fallback/" + [version_name, "default.jpg"].compact.join('_')
   # end
 
   # Process files as they are uploaded:
@@ -31,6 +31,10 @@ class Image1Uploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+     # def default_url
+     #   'default.jpg'
+     # end
+
   # Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_limit => [200, 200]
@@ -38,9 +42,9 @@ class Image1Uploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
