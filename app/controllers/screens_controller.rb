dc3edc44359520297cf65e26fc028a000b3ebe21
sanjeev <sanjeev.impinge@gmail.com>
2014-11-24 15:14:29 +0530

    Added screen3 for properties details Added different delays for different pages

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -14,5 +14,10 @@ class ScreensController < ApplicationController
 
 	def screen2
 		render '/screens/screen2/screen2', :layout => "screen_layout"
+	end
+
+	def screen3
+		@properties = Property.limit(5)
+		render '/screens/screen3/screen3', :layout => "screen_layout"
 	end	
 end
