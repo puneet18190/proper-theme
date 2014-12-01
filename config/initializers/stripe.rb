1e03031f7c456bc944ba9b4df3c698c5e18a08bc
sanjeev <sanjeev.impinge@gmail.com>
2014-12-01 16:45:48 +0530

    Change stripe api keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = "sk_test_qqfegpLWKFzXLzsxHSVnIZ5V"
-STRIPE_PUBLIC_KEY = "pk_test_Jf9qcjcAsmgiM1WqN71owSyz"
+Stripe.api_key = "sk_live_If3CpEHPsD1YCiOJ5o82vSO1"
+STRIPE_PUBLIC_KEY = "pk_live_YVX4t05m5whd28bzxLlo4n3p"
