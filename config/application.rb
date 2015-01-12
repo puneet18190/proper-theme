b85c549101bcf52b1a80a3fceec6da18f8edd43c
Puneet <puneetgupta18190@gmail.com>
2015-01-12 23:33:59 +0530

    test

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,33 @@ module Proper
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w( /bootstrap-tooltip.js )
+    config.assets.precompile += %w( /Grid-A-Licious-master/jquery.grid-a-licious.min.js )
+    config.assets.precompile += %w( /carouFredSel-6.2.1/jquery.carouFredSel-6.2.1-packed.js )
+    config.assets.precompile += %w( /carouFredSel-6.2.1/helper-plugins/jquery.mousewheel.min.js )
+    config.assets.precompile += %w( /carouFredSel-6.2.1/helper-plugins/jquery.touchSwipe.min.js )
+    config.assets.precompile += %w( /carouFredSel-6.2.1/helper-plugins/jquery.transit.min.js )
+    config.assets.precompile += %w( /carouFredSel-6.2.1/helper-plugins/jquery.ba-throttle-debounce.min.js )
+    config.assets.precompile += %w( /gmap3/gmap3.min.js )
+    config.assets.precompile += %w( /colorbox/jquery.colorbox-min.js )
+    config.assets.precompile += %w( /jquery.js )
+    config.assets.precompile += %w( /bootstrap-transition.js )
+    config.assets.precompile += %w( /bootstrap-alert.js )
+    config.assets.precompile += %w( /bootstrap-modal.js )
+    config.assets.precompile += %w( /bootstrap-dropdown.js )
+    config.assets.precompile += %w( /bootstrap-scrollspy.js )
+    config.assets.precompile += %w( /bootstrap-tab.js )
+    
+    config.assets.precompile += %w( /bootstrap-popover.js )
+    config.assets.precompile += %w( /bootstrap-button.js )
+    config.assets.precompile += %w( /bootstrap-collapse.js )
+    config.assets.precompile += %w( /bootstrap-carousel.js )
+    config.assets.precompile += %w( /bootstrap-typeahead.js )
+    config.assets.precompile += %w( /spin.min.js )
+    config.assets.precompile += %w( /proper.js )
+    config.assets.precompile += %w( /bootstrap.min.css")
+  config.assets.precompile += %w( /bootstrap-responsive.min.css)
+  config.assets.precompile += %w( /style.css)
+  config.assets.precompile += %w( /colorbox/colorbox.css)
   end
 end
