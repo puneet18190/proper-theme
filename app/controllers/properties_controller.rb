8da4fa01037c3db3ce1eea27aaf8e86c19d80e43
sanjeev <sanjeev.impinge@gmail.com>
2015-03-16 14:39:35 +0530

    tenant searching

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -381,16 +381,18 @@ class PropertiesController < ApplicationController
     @news = News.all
     @properties.each do |p|
       @tenants.each do |t|
-        @string = t.search
-        @words = @string.split(/\W+/)
-        @category = @words[1]
-        @price_l = @words[2]
-        @price_g = @words[3]
-        @beds = @words[4]
-        @bath = @words[5]
-        if p.category.include?(@category) && (@price_l.to_i..@price_g.to_i).cover?(p.price) && p.beds.equal?(@beds.to_i) && p.bath.equal?(@bath.to_i)
-          @data << t
-        end  
+        unless t.search.nil?
+          @string = t.search
+          @words = @string.split(/\W+/)
+          @category = @words[1]
+          @price_l = @words[2]
+          @price_g = @words[3]
+          @beds = @words[4]
+          @bath = @words[5]
+          if p.category.include?(@category) && (@price_l.to_i..@price_g.to_i).cover?(p.price) && p.beds.equal?(@beds.to_i) && p.bath.equal?(@bath.to_i)
+            @data << t
+          end 
+        end 
       end
     end    
   end  
