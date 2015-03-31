8672cb00db478f3ccc6a568b6140c734b0e7944c
Puneet <puneetgupta18190@gmail.com>
2015-03-31 15:21:34 +0530

    reverted code

--- app/models/screen.rb
+++ app/models/screen.rb
@@ -10,12 +10,12 @@
 #
 
 class Screen < ActiveRecord::Base
-	def self.save_file(upload)
-	    name =  upload['datafile'].original_filename
-	    directory = "public/doc"
-	    # create the file path
-	    path = File.join(directory, name)
-	    # write the file
-	    File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
-	  end
+	# def self.save_file(upload)
+	#     name =  upload['datafile'].original_filename
+	#     directory = "public/doc"
+	#     # create the file path
+	#     path = File.join(directory, name)
+	#     # write the file
+	#     File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+	#   end
 end
