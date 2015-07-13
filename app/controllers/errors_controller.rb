9150acc0e4bd47e15d56a2933532a774674ba586
Puneet <puneetgupta18190@gmail.com>
2015-07-14 00:18:49 +0530

    mobile

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,17 @@
 class ErrorsController < ApplicationController
   def not_found
-    render(:status => 404)
+  	if is_mobile_device?
+    	render(:status => 404), layout: "mobile"
+    else
+    	render(:status => 404)
+    end
   end
 
   def internal_server_error
-    render(:status => 500)
+    if is_mobile_device?
+    	render(:status => 500), layout: "mobile"
+    else
+    	render(:status => 500)
+    end
   end
 end
