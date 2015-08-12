c0d2433fb1c67a671f47ed50bbb973b07bcacdc0
Puneet <puneetgupta18190@gmail.com>
2015-08-12 23:20:26 +0530

    add prefill email on signin form after confirm user

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,14 +1,14 @@
 class ConfirmationsController < Devise::ConfirmationsController
   def after_confirmation_path_for(resource_name, resource)
-    if resource[:status] == "landlord"
-      @con = RubyZoho::Crm::Contact.new(
-          :last_name => resource[:first_name],
-          :email => resource[:email],
-          :phone => resource[:phone],
-          :mailing_street => resource[:address]
-      )
-    @con.save
-    end
-    new_user_session_path
+    # if resource[:status] == "landlord"
+    #   @con = RubyZoho::Crm::Contact.new(
+    #       :last_name => resource[:first_name],
+    #       :email => resource[:email],
+    #       :phone => resource[:phone],
+    #       :mailing_street => resource[:address]
+    #   )
+    # @con.save
+    # end
+    new_user_session_path("user[email]" => params["user"]["email"])
   end
 end
