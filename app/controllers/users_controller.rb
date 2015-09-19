d90ea6c6c4ffa8f25a7ddeb84ac06af13a5c5517
Puneet <puneetgupta18190@gmail.com>
2015-09-20 01:37:25 +0530

    Made profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -137,6 +137,10 @@ class UsersController < ApplicationController
 		end	
     end
 
+    def show
+    	@user = User.find params[:id]
+    end
+
     private
     def set_user
       @user = User.find(params[:id])
