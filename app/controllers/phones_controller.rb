01ce446e7e3b6fce2928e278d26c4fa1072b4d2f
Puneet <puneetgupta18190@gmail.com>
2016-03-01 21:40:50 +0530

    phone parameters

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,7 +1,7 @@
 class PhonesController < ApplicationController
   
   def help
-    @data = Phone.new(callerid: params[:call_id], call_action: params[:call_action], dataname: params[:dataname])
+    @data = Phone.new(callerid: params[:call_id], call_action: params[:action], dataname: params[:mac])
     @data.save
     render :nothing => true
   end
