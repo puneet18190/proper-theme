c604e183f2a26ee73a62851ceb8c74509c586ca3
Puneet <puneetgupta18190@gmail.com>
2016-03-06 00:24:12 +0530

    phone

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,15 +1,15 @@
 class PhonesController < ApplicationController
   
   def help
-  	if params[:mac] == "0015654c66c2"
-  		mac= "Emma's Shop"
-  	elsif params[:mac] == "0015654c6db2"
-  		mac = "Steve's Shop"
-  	else
-  		mac=""
-  	end
+  	# if params[:mac] == "0015654c66c2"
+  	# 	mac= "Emma's Shop"
+  	# elsif params[:mac] == "0015654c6db2"
+  	# 	mac = "Steve's Shop"
+  	# else
+  	# 	mac=""
+  	# end
   		
-    @data = Phone.new(callerid: params[:call_id], call_action: params[:call_action], dataname: mac, 
+    @data = Phone.new(callerid: params[:call_id], call_action: params[:call_action], dataname: params[:mac], 
       local: params[:local], remote: params[:remote], 
       display_local: params[:display_local], display_remote: params[:display_remote]
     )
