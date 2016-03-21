2ebdd5411d197542853236784ddda49d69a6840e
Puneet <puneetgupta18190@gmail.com>
2016-03-21 22:15:02 +0530

    csrf token error

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,4 +1,5 @@
 class SmsController < ApplicationController
+	protect_from_forgery :only => ["send_sms"]
 
 	def send_sms
 		@sms = Sms.new(originator: params[:originator], destination: params[:destination], message: params[:message])
