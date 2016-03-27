15a000266fc14203fe770d7d7234e847488632fe
Puneet <puneetgupta18190@gmail.com>
2016-03-27 19:46:32 +0530

    added sms received functionalit on sticky
    pusher for received sms
    corrected date format on sticky page

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -3,6 +3,18 @@ class SmsController < ApplicationController
 
 	def send_sms
 		@sms = Sms.new(originator: params[:originator], destination: params[:destination], message: params[:message])
+		@user = User.where("mobile = ? OR phone =?", params[:originator],params[:originator]).first
+		name = @user.blank? ? "" : @user.first_name
+		user_type = @user.blank? ? "" : @user.status[0].capitalize
+
+		Pusher['private'].trigger('receive_sms', {
+			caller_id: params[:originator], 
+			name: name, 
+			message: params[:message], 
+			received_date: DateTime.now.strftime("%d/%m/%Y"),
+			received_time: DateTime.now.strftime("%T"),
+			user_type: user_type
+		})
 		if @sms.save
 			render :json => {status: true}
 		else
